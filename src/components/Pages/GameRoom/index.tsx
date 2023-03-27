61677599239ceec3deea5f2c39f4d5f2b38c8052
ntv2002a <trungvu16502@gmail.com>
2023-03-27 20:21:56 +0700

    . update gameroom p2

--- src/components/Pages/GameRoom/index.tsx
+++ src/components/Pages/GameRoom/index.tsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import { ProSidebarProvider } from 'react-pro-sidebar';
-import { GlobalContext, SocketContext } from '../../../App';
+import { ApplicationContext, GlobalContext, SocketContext } from '../../../App';
 import { Player } from '../../../type';
 import { SideBar } from '../../Navigation/SideBar';
+import { getNumberOfPlayers, getPlayerCurrentPositionInRoom } from '../../utils';
+import { PlayerResult } from './Action/PlayerResult';
 import "./styles.css";
 
 export const GameRoom = () => {
@@ -22,49 +24,90 @@ export const GameRoom = () => {
 
     let otherPlayer: Player[] | undefined = globalContext?.currentRoom?.players.filter(players => players.socketUser.socketId !== socket.id);
 
+    //TeacherStarCi
+    const [yourAddress, setYourAddress] = React.useState('');
+    const [otherAddresses, setOtherAddresses] = React.useState<string[]>([]);
+    const context = React.useContext(ApplicationContext);
+    React.useEffect(() => {
+        const assignOtherAddressess: string[] = [];
+        if (context == null) return;
+        if (context.getter.roomSet == null
+            || context.getter.currentRoom == null
+            || context.getter.userWithBalance == null) return;
+        const numberOfPlayers = getNumberOfPlayers(context.getter.currentRoom.code, context.getter.roomSet);
+        if (numberOfPlayers < 0) return;
+        setYourAddress(context.getter.userWithBalance.user.address);
+        if (numberOfPlayers < 2) return;
+        const playerIndex = getPlayerCurrentPositionInRoom(context.getter.userWithBalance.user.address, context.getter.roomSet)
+        for (let i = 0; i < numberOfPlayers; i++){
+           if (i !== playerIndex){
+            assignOtherAddressess.push(context.getter.currentRoom.players[i].socketUser.user.address)
+           }
+        }
+        setOtherAddresses(assignOtherAddressess);
+    }, [context])
+
+    const displayOpponents = (otherAddresses: string[]): JSX.Element => {
+        const results: JSX.Element[] = [];
+        otherAddresses.forEach((address: string) => {
+            results.push(<PlayerResult {...{ address: address }}>
+                </PlayerResult>)
+        })
+        return (<div className = 'DisplayOpponents'> {results} </div>)
+    }
+
     if (globalContext) {
+        // return (
+        //     <div className='App-dashboard'>
+        //         <div>
+        //             <ProSidebarProvider>
+        //                 <SideBar />
+        //             </ProSidebarProvider>
+        //             <div>
+        //                 <div className='div-0'>
+        //                     <div className='div-1'>
+        //                         <div className='div-player-1'>
+        //                             User: {otherPlayer?.at(0)?.socketUser.user.username} <br />
+        //                             {(typeof otherPlayer?.at(0)?.hand !== 'undefined') && otherPlayer.at(0)?.hand?.cards.toString()}
+        //                         </div>
+        //                     </div>
+        //                     <div className='div-2'>
+        //                         <div className='div-player-2'>
+        //                             User: {otherPlayer?.at(1)?.socketUser.user.username} <br />
+        //                             {(typeof otherPlayer?.at(1)?.hand !== 'undefined') && otherPlayer.at(1)?.hand?.cards.toString()}
+        //                         </div>
+        //                         <div className='div-player-3'>
+        //                             User: {otherPlayer?.at(2)?.socketUser.user.username} <br />
+        //                             {(typeof otherPlayer?.at(2)?.hand !== 'undefined') && otherPlayer.at(2)?.hand?.cards.toString()}
+        //                         </div>
+        //                     </div>
+        //                     <div className='div-1'>
+        //                         <div className='div-player-4'>
+        //                             User: {mainPlayer?.socketUser.user.username} <br />
+        //                             {/* In ra lá bài */}
+        //                             {/* <img src={logo} className="cards" alt='backcard'></img>
+        //                             <img src={logo} className="cards" alt='backcard'></img>
+        //                             <img src={logo} className="cards" alt='backcard'></img> */}
+        //                             {(typeof mainPlayer?.hand !== 'undefined') && (
+        //                                 mainPlayer.hand.cards.map((x, index) => <img key={index} src={x.cardName + ' ' + x.suit + '.png'} className="cards" alt={x.cardName + ' ' + x.suit}></img>)
+        //                             )}
+        //                             <br />
+        //                             <button onClick={() => startGame(globalContext.currentRoom?.code)}>Start Game</button>
+        //                             <button onClick={() => terminateGame(globalContext.currentRoom?.code)}>Terminate</button>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // )
         return (
-            <div className='App-dashboard'>
-                <div>
-                    <ProSidebarProvider>
-                        <SideBar />
-                    </ProSidebarProvider>
-                    <div>
-                        <div className='div-0'>
-                            <div className='div-1'>
-                                <div className='div-player-1'>
-                                    User: {otherPlayer?.at(0)?.socketUser.user.username} <br />
-                                    {(typeof otherPlayer?.at(0)?.hand !== 'undefined') && otherPlayer.at(0)?.hand?.cards.toString()}
-                                </div>
-                            </div>
-                            <div className='div-2'>
-                                <div className='div-player-2'>
-                                    User: {otherPlayer?.at(1)?.socketUser.user.username} <br />
-                                    {(typeof otherPlayer?.at(1)?.hand !== 'undefined') && otherPlayer.at(1)?.hand?.cards.toString()}
-                                </div>
-                                <div className='div-player-3'>
-                                    User: {otherPlayer?.at(2)?.socketUser.user.username} <br />
-                                    {(typeof otherPlayer?.at(2)?.hand !== 'undefined') && otherPlayer.at(2)?.hand?.cards.toString()}
-                                </div>
-                            </div>
-                            <div className='div-1'>
-                                <div className='div-player-4'>
-                                    User: {mainPlayer?.socketUser.user.username} <br />
-                                    {/* In ra lá bài */}
-                                    {/* <img src={logo} className="cards" alt='backcard'></img>
-                                    <img src={logo} className="cards" alt='backcard'></img>
-                                    <img src={logo} className="cards" alt='backcard'></img> */}
-                                    {(typeof mainPlayer?.hand !== 'undefined') && (
-                                        mainPlayer.hand.cards.map((x, index) => <img key={index} src={x.cardName + ' ' + x.suit + '.png'} className="cards" alt={x.cardName + ' ' + x.suit}></img>)
-                                    )}
-                                    <br />
-                                    <button onClick={() => startGame(globalContext.currentRoom?.code)}>Start Game</button>
-                                    <button onClick={() => terminateGame(globalContext.currentRoom?.code)}>Terminate</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className='GameRoom'>
+                <h1> YOU </h1>
+                <PlayerResult {...{ address: yourAddress }}>
+                </PlayerResult>
+                <h1> OPPONENT(S) </h1>
+                {displayOpponents(otherAddresses)}
             </div>
         )
     }
